6a9c5deb44a05130a1fe4d5ab42e01fc61106237
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-05 00:47:19 +0300

    Web Service Build

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,5 +10,19 @@ router.post('/', (req, res, next) => {
         message: 'Handling POST request to /products'
     });
 });
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id == 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
 module.exports = router;
  
