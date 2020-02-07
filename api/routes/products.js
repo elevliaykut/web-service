988668ed09c6a1e78dddbcc53b6454de9f00114e
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-07 15:12:33 +0300

    Restful method build with mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,9 +4,25 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            // if (docs.length >= 0) {
+            res.status(200).json(docs);
+            // } else {
+            //    res.status(404).json({
+            //        message: 'No entries found'
+            //   });
+            //}
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 //Veriyi güvenilir şekilde http ile gönderir.
 router.post('/', (req, res, next) => {
@@ -56,14 +72,36 @@ router.get('/:productId', (req, res, next) => {
 });
 //Veriyi günceller.
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Update Product!'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(200).json({
+                error: err
+            })
+        })
 });
 //Veriyi Siler.
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product!'
-    });
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 module.exports = router;
