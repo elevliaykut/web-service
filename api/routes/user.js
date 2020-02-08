9da6525a87b04e6ac702a4ceaef9e9d501d64acc
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-09 02:13:43 +0300

    Regex to email

--- api/routes/user.js
+++ api/routes/user.js
@@ -37,13 +37,27 @@ router.post('/signup', (req, res, next) => {
                                 res.status(500).json({
                                     error: err,
                                     message: 'User Not Created'
-                                })
-                            })
+                                });
+                            });
                     }
-                })
+                });
             }
+        });
+});
+
+router.delete('/:userId', (req,res,next) => {
+    User.remove({ _id: req.params.userId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message:'User Deleted Succsessfully'
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
         })
-
-
+    });
 })
 module.exports = router;
