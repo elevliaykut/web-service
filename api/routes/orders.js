0cb2d856eeb6bcddca0eef8a62d47229769c37c6
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-08 15:20:51 +0300

    Order model build

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -7,14 +10,26 @@ router.get('/', (req, res, next) => {
     });
 });
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Orders were created',
-        order: order
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
     });
+    order
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Order was created',
+                order: order
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 router.get('/:orderId', (req, res, next) => {
     res.status(201).json({
