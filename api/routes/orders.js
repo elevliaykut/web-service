1530eb2e7c3fcae0f2fce3c53678744a95446afd
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-08 16:10:10 +0300

    Order Apı Build

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,31 @@ const mongoose = require('mongoose');
 const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
+    Order.find()
+        .select('product quantity _id') //ony get this properties
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        _id: doc.id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: 'https://localhost:3000/orders/' + doc._id,
+                            description: 'See details to Order Id = ' + doc._id
+                        }
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 router.post('/', (req, res, next) => {
     const order = new Order({
@@ -21,12 +43,21 @@ router.post('/', (req, res, next) => {
             console.log(result);
             res.status(201).json({
                 message: 'Order was created',
-                order: order
-            })
+                createdOrder:{
+                    _id:result._id,
+                    product:result.product,
+                    quantity:result.quantity
+                },
+                request:{
+                    type:'POST',
+                    url:'https://localhost:3000/orders/' + result._id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
+                message:'Order Not Created',
                 error: err
             })
         });
