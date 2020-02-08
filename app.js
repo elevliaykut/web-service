55c82ba380e5f91eda4bb7b921ca4851f673d549
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-09 01:16:33 +0300

    User SignUp build to Apı

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
     'mongodb+srv://aykut:' +
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error("Not Found");
