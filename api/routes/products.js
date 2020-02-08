8396edf119adc22643f7cfe16c39ca7c95223d39
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-08 14:18:35 +0300

    Validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -11,13 +11,13 @@ router.get('/', (req, res, next) => {
             const response = {
                 count: docs.length,
                 products: docs.map(doc => {
-                    return{
-                        name:doc.name,
-                        price:doc.price,
-                        _id:doc._id,
-                        request:{
-                            type:'GET',
-                            url:'https://localhost:3000/products/' +doc._id
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'localhost:3000/products/' + doc._id
                         }
                     }
                 })
@@ -51,8 +51,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST request to /products',
-                createdProduct: result
+                message: 'Created product succesfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'POST',
+                        url: 'https://localhost:3000/products/' + result._id
+                    }
+                }
             });
         })
         .catch(err => {
@@ -66,11 +74,20 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log("From Database", doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'https://localhost:3000/products/'
+                    }
+                })
             }
             else {
                 res.status(404).json({
@@ -95,8 +112,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product Updated',
+                request: {
+                    type: 'PATCH',
+                    url: 'https://localhost:3000/products/' + id
+                }
+            })
         })
         .catch(err => {
             console.log(err);
@@ -111,7 +133,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product Deleted',
+                request: {
+                    type: 'DELETE',
+                    url: 'https://localhost:3000/products/',
+                    body: { name: 'String', price: 'Number' }
+                }
+            })
         })
         .catch(err => {
             res.status(500).json({
--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect('mongodb+srv://aykut:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-vk09i.mongodb.net/test?retryWrites=true&w=majority', {
-    useMongoClient: true
-})
-
+mongoose.connect(
+    'mongodb+srv://aykut:' +
+    process.env.MONGO_ATLAS_PW +
+    '@node-rest-shop-vk09i.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useMongoClient: true
+    }
+);
+mongoose.Promise = global.Promise;
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
