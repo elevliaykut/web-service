658a2273ab1b4cccb95eb842d51e966c9eebc805
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-08 22:49:51 +0300

    Upload Image to Server

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
@@ -40,7 +42,8 @@ router.get('/', (req, res, next) => {
         });
 });
 //Veriyi güvenilir şekilde http ile gönderir.
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+    console.log(req.file); 
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
