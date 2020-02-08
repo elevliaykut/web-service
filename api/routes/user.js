06896ab7690f43a2ab0f647b69c6239a0fc0e35f
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-09 01:49:07 +0300

    SignUp validation build

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,33 +6,44 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message:'User Created Successfully'
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail exist'
                 });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error:err,
-                    message:'User Not Created'
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User Created Successfully'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err,
+                                    message: 'User Not Created'
+                                })
+                            })
+                    }
                 })
-            })
-        }
-    })
+            }
+        })
+
 
 })
 module.exports = router;
