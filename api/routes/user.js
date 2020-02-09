4e6d412acc1f7dfbe02fb58848d9a3b4552d2ba9
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-09 10:09:17 +0300

    Authentication Build on Web Service

--- api/routes/user.js
+++ api/routes/user.js
@@ -45,19 +45,52 @@ router.post('/signup', (req, res, next) => {
         });
 });
 
-router.delete('/:userId', (req,res,next) => {
-    User.remove({ _id: req.params.userId})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message:'User Deleted Succsessfully'
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(404).json({
+                    message:'Authentication Failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err){
+                    return res.status(500).json({
+                        message:'Authentication Failed'
+                    });
+                }
+                if(result){
+                    return res.status(200).json({
+                        message:'Authentication Successful'
+                    });
+                }
+                res.status(404).json({
+                    message:'Authentication Failed'
+                })
+            });
         })
-    });
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+})
+
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'User Deleted Succsessfully'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 })
 module.exports = router;
