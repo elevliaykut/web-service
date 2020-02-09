795d1b99642cb8884217f2e604d80cd598bdb691
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-09 11:06:28 +0300

    Auth control & Verify to Routes

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,15 +1,14 @@
-const jwt = req('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
         next();
     } catch (error) {
         return res.status(401).json({
-            message:'Auth failed'
+            message: 'Auth failed'
         })
     }
-    next();
-
 };
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -39,7 +40,7 @@ router.get('/', (req, res, next) => {
                     return {
                         name: doc.name,
                         price: doc.price,
-                        productImage:doc.productImage,
+                        productImage: doc.productImage,
                         _id: doc._id,
                         request: {
                             type: 'GET',
@@ -66,7 +67,7 @@ router.get('/', (req, res, next) => {
         });
 });
 //Veriyi güvenilir şekilde http ile gönderir.
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
     console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -131,7 +132,7 @@ router.get('/:productId', (req, res, next) => {
         });
 });
 //Veriyi günceller.
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -156,7 +157,7 @@ router.patch('/:productId', (req, res, next) => {
         })
 });
 //Veriyi Siler.
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
         .exec()
